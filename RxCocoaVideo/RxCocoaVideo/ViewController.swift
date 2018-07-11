97d230a897321a13929c9fbc92a6ea57bf024292
胡涛 <hutao@hutaodemac-mini.local>
2018-07-11 20:59:26 +0800

    init

---
+++ RxCocoaVideo/RxCocoaVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxCocoaVideo
+//
+//  Created by 胡涛 on 2018/7/11.
+//  Copyright © 2018年 胡涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
